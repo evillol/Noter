196728c79d89f672b7f79add5f277eadae06d98d
zaqwer <platonovza@ya.ru>
2015-02-21 18:31:53 +0900

    second commit

--- EventManager/EventManager.cs
+++ EventManager/EventManager.cs
@@ -8,5 +8,15 @@ namespace EventManager
     public static class EventManager
     {
         public static List<Event> events = new List<Event>();
+
+        public static void Remove(int index)
+        {
+            EventManager.events.RemoveAt(index);
+        }
+
+        public static void Remove(Event ev)
+        {
+            EventManager.events.Remove(ev);
+        }
     }
 }
