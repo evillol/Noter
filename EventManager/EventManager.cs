d05e966eb8cddc48fb03da951c300731588fbb5c
zaqwer <platonovza@ya.ru>
2015-02-21 17:50:19 +0900

    first commit

---
+++ EventManager/EventManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventManager
+{
+    public static class EventManager
+    {
+        public static List<Event> events = new List<Event>();
+    }
+}
