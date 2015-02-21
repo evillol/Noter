19aa8651ecc653b716454b435a4a5aaeeb9e1f38
zaqwer <platonovza@ya.ru>
2015-02-21 19:50:37 +0900

    третий commit

--- EventManager/Event.cs
+++ EventManager/Event.cs
@@ -5,7 +5,28 @@ using System.Text;
 
 namespace EventManager
 {
-    class Event
+    public class Event
     {
+        private string name, details;
+        private int id;
+        private DateTime time;
+
+        public Event(string name, DateTime time, string details, int id)
+        {
+            this.details = details;
+            this.name = name;
+            this.time = time;
+            this.id = id;
+        }
+
+       /* public DateTime Check()
+        {
+            DateTime dt;
+            dt = DateTime.Now;
+            long ticks;
+            ticks = (this.time - dt).Ticks;
+            return new DateTime(ticks);
+        }
+       */
     }
 }
