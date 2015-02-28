cfff88cad95800c9bb871d9e152143e705a6de37
zaqwer <platonovza@ya.ru>
2015-02-28 14:47:44 +0900

    afsfaf

--- EventManager/EventManager.cs
+++ EventManager/EventManager.cs
@@ -8,6 +8,30 @@ namespace EventManager
     public static class EventManager
     {
         public static List<Event> events = new List<Event>();
-		//тут был Hozren
+
+        public static void Remove(int index)
+        {
+            EventManager.events.RemoveAt(index);            //Удаление по номеру
+        }           
+
+        public static void Remove(Event ev)                 //Удаление по элементу
+        {
+            EventManager.events.Remove(ev);
+        }                   
+
+        public static Event Check_first()                   //Поиск ближайшего события
+        {
+            DateTime dt = events[0].time;
+            int number = 0;
+            for(int i=1;i<events.Count;i++)
+            {
+                if(events[i].Check() < dt)
+                {
+                    number = i;
+                    dt = events[i].time;
+                }
+            }
+            return events[number];
+        }
     }
 }
